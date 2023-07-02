8717fe7e3203f9bbb048caa225c14e27d3bddffe
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-02 18:29:25 -0400

    Funny arch API moment
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/GenerationsStructuresForge.java
+++ forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/GenerationsStructuresForge.java
@@ -1,6 +1,5 @@
 package generations.gg.generations.structures.generationsstructures.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import generations.gg.generations.structures.generationsstructures.GenerationsStructures;
 import generations.gg.generations.structures.generationsstructures.forge.integration.BOP;
 import generations.gg.generations.structures.generationsstructures.forge.integration.BYGBOP;
@@ -9,13 +8,10 @@ import generations.gg.generations.structures.generationsstructures.integration.D
 import generations.gg.generations.structures.generationsstructures.integration.Integration;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(GenerationsStructures.MOD_ID)
 public class GenerationsStructuresForge {
     public GenerationsStructuresForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(GenerationsStructures.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Integration integration;
         if (ModList.get().isLoaded("biomesoplenty") && ModList.get().isLoaded("byg")) integration = new BYGBOP();
         else if (ModList.get().isLoaded("biomesoplenty")) integration = new BOP();
