2010a35933a83428aab60aad5a0a4fe6ae445200
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-03 10:28:39 -0400

    Forgot about the Duel class
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/integration/BYGBOP.java
+++ forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/integration/BYGBOP.java
@@ -1,10 +1,24 @@
 package generations.gg.generations.structures.generationsstructures.forge.integration;
 
+import biomesoplenty.core.BiomesOPlenty;
 import generations.gg.generations.structures.generationsstructures.integration.Integration;
+import net.minecraft.world.level.block.Block;
+import potionstudios.byg.BYG;
+import potionstudios.byg.common.block.BYGWoodTypes;
 
 public class BYGBOP implements Integration {
     @Override
     public String getModId() {
-        return "bygbop";
+        return BYG.MOD_ID + BiomesOPlenty.MOD_ID;
+    }
+
+    @Override
+    public Block getBirchTrapdoorReplacement() {
+        return BYGWoodTypes.WITCH_HAZEL.trapdoor().get();
+    }
+
+    @Override
+    public Block getGhostTrapdoorReplacement() {
+        return BYGWoodTypes.EBONY.trapdoor().get();
     }
 }
