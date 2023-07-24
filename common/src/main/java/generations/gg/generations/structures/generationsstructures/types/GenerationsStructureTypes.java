bfbd03f0e27f3004e67e9db0df33000aa6b3c3f2
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-23 23:02:07 -0400

    Start EndJigsawStructure.java
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

---
+++ common/src/main/java/generations/gg/generations/structures/generationsstructures/types/GenerationsStructureTypes.java
@@ -0,0 +1,11 @@
+package generations.gg.generations.structures.generationsstructures.types;
+
+import net.minecraft.world.level.levelgen.structure.StructureType;
+
+public class GenerationsStructureTypes {
+
+    /* All custom structure types */
+    public static StructureType<EndJigsawStructure> END_JIGSAW = () -> EndJigsawStructure.CODEC;
+
+
+}
