2b63193736b353621183cdd29a20b7ba88790867
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-28 10:51:23 -0500

    Move Structures to match where the key location is.

--- common/src/main/java/generations/gg/generations/structures/generationsstructures/worldgen/template_pool/GenerationsTemplatePools.java
+++ common/src/main/java/generations/gg/generations/structures/generationsstructures/worldgen/template_pool/GenerationsTemplatePools.java
@@ -3,6 +3,7 @@ package generations.gg.generations.structures.generationsstructures.worldgen.tem
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import generations.gg.generations.structures.generationsstructures.GenerationsStructures;
+import generations.gg.generations.structures.generationsstructures.structures.GenerationsStructuresKeys;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -31,41 +32,41 @@ public class GenerationsTemplatePools {
 
     public static void bootstrap(BootstapContext<StructureTemplatePool> context) {
         register(context, BEAST_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("loot_balloon/beast_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.BEAST_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, GREAT_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("loot_balloon/great_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.GREAT_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, MASTER_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("loot_balloon/master_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.MASTER_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, MEOWTH_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("loot_balloon/meowth_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.MEOWTH_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, NORMAL_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("normal_balloon/beast_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.NORMAL_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, ULTRA_BALLOON, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("loot_balloon/ultra_balloon"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.ULTRA_BALLOON.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
 
         register(context, COMET, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("comet"), getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.COMET.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
         register(context, SCARLET_POKESHOP, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single(getOverworldPath("scarlet_pokeshop")), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.SCARLET_POKESHOP.location().toString()), 1)
         ), StructureTemplatePool.Projection.RIGID);
 
 
         register(context, SPIKE, Pools.EMPTY, ImmutableList.of(
-                Pair.of(StructurePoolElement.single("generations_structures:nether/spike", getProcessor(context, ProcessorLists.EMPTY)), 1)
+                Pair.of(StructurePoolElement.single(GenerationsStructuresKeys.SPIKE.location().toString(), getProcessor(context, ProcessorLists.EMPTY)), 1)
         ), StructureTemplatePool.Projection.TERRAIN_MATCHING);
     }
 
@@ -75,10 +76,6 @@ public class GenerationsTemplatePools {
 
     private static void register(BootstapContext<StructureTemplatePool> context, ResourceKey<StructureTemplatePool> key, ResourceKey<StructureTemplatePool> fallback, List<Pair<Function<StructureTemplatePool.Projection, ? extends StructurePoolElement>, Integer>> rawTemplateFactories, StructureTemplatePool.Projection projection) {
         context.register(key, new StructureTemplatePool(context.lookup(Registries.TEMPLATE_POOL).getOrThrow(fallback), rawTemplateFactories, projection));
-    }
-
-    private static String getOverworldPath(String name) {
-        return "generations_structures:overworld/" + name;
     }
 
     private static Holder.Reference<StructureProcessorList> getProcessor(BootstapContext<StructureTemplatePool> context, ResourceKey<StructureProcessorList> processorList) {
