72c9e72822c1e940f6e103cb3efc57bd56cd6e77
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-29 09:28:15 -0500

    Fabric can now generate structures in villages (I hope)

--- fabric/src/main/java/generations/gg/generations/structures/generationsstructures/fabric/GenerationsStructuresFabric.java
+++ fabric/src/main/java/generations/gg/generations/structures/generationsstructures/fabric/GenerationsStructuresFabric.java
@@ -3,7 +3,9 @@ package generations.gg.generations.structures.generationsstructures.fabric;
 import generations.gg.generations.structures.generationsstructures.GenerationsStructures;
 import generations.gg.generations.structures.generationsstructures.integration.Default;
 import generations.gg.generations.structures.generationsstructures.processors.StructureProcessors;
+import generations.gg.generations.structures.generationsstructures.village.PlaceInVillage;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 /**
  * Fabric initializer for Generations Structures
@@ -16,5 +18,6 @@ public class GenerationsStructuresFabric implements ModInitializer {
     public void onInitialize() {
         GenerationsStructures.init(new Default());  //TODO: FabricLoader.getInstance().isModLoaded(potionstudios.byg.BYG.MOD_ID) ? new BYG() : new Default()
         StructureProcessors.init();
+        ServerLifecycleEvents.SERVER_STARTING.register(PlaceInVillage::addStructuresToVillages);
     }
 }
