6a4fd8cf3e1627fb720bf3fbf91c50889248e52c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-12-23 12:38:15 -0500

    Register this event the right way

--- forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/events/LifeCycleEvents.java
+++ forge/src/main/java/generations/gg/generations/structures/generationsstructures/forge/events/LifeCycleEvents.java
@@ -1,17 +1,12 @@
 package generations.gg.generations.structures.generationsstructures.forge.events;
 
-import generations.gg.generations.structures.generationsstructures.GenerationsStructures;
 import generations.gg.generations.structures.generationsstructures.village.PlaceInVillage;
 import net.minecraftforge.event.server.ServerAboutToStartEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = GenerationsStructures.MOD_ID)
 public class LifeCycleEvents {
 	/**
 	 * Adds all custom structures to villages.
 	 */
-	@SubscribeEvent
 	public static void aboutToStartEvent(final ServerAboutToStartEvent event) {
 		PlaceInVillage.addStructuresToVillages(event.getServer());
 	}
