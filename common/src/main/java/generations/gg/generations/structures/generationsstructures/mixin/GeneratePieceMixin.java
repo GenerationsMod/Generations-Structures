b0b12b145a7b9084709c96a873eb63f8901efb1f
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-02-05 14:01:28 -0500

    Now we sprinkle in some randomization

--- common/src/main/java/generations/gg/generations/structures/generationsstructures/mixin/GeneratePieceMixin.java
+++ common/src/main/java/generations/gg/generations/structures/generationsstructures/mixin/GeneratePieceMixin.java
@@ -2,11 +2,11 @@ package generations.gg.generations.structures.generationsstructures.mixin;
 
 import generations.gg.generations.structures.generationsstructures.GenerationsStructures;
 import generations.gg.generations.structures.generationsstructures.village.VanillaVillages;
-import generations.gg.generations.structures.generationsstructures.worldgen.template_pool.GenerationsTemplatePools;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import org.joml.Random;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,19 +23,39 @@ public abstract class GeneratePieceMixin {
     @Shadow
     private Registry<StructureTemplatePool> pools;
 
+    @Unique
+    private static int randomCheck = 0;
+
+    @Unique
+    private static int randomInt = -1;
+
 
     @ModifyArg(method = "tryPlacingChildren", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;getHolder(Lnet/minecraft/resources/ResourceKey;)Ljava/util/Optional;"))
     private ResourceKey<StructureTemplatePool> limitVillageStructures(ResourceKey<StructureTemplatePool> resourceKey) {
         if (hasGenerationsStructure || !GenerationsStructures.CONFIG.villageStructureGeneration.AllowStructuresInVillages)
             return resourceKey;
 
+        GenerationsStructures.LOGGER.info("randomInt = " + randomInt);
+        if (randomInt == -1) {
+            randomInt = new Random().nextInt(4);
+            randomCheck = 0;
+        }
+
         String poolPath = resourceKey.location().getPath();
         if (poolPath.endsWith("/streets")) {
+            if (randomCheck != randomInt) {
+                randomCheck++;
+                return resourceKey;
+            }
             VanillaVillages village;
             if ((village = VanillaVillages.containsName(poolPath)) != null) {
+                if (village != VanillaVillages.PLAINS && village != VanillaVillages.DESERT)
+                    return resourceKey;
                 ResourceKey<StructureTemplatePool> generationsPoolKey = village.getPool();
                 if (pools.getHolder(generationsPoolKey).isPresent()) {
                     hasGenerationsStructure = true;
+                    randomInt = -1;
+                    randomCheck = 0;
                     return generationsPoolKey;
                 }
             }
